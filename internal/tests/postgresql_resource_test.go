8b7e9c25d9b685df7829acd22ffa13cd4bb6bc1f
CezaryDwojak <cezary.2.dwojak.external@atos.net>
2026-04-10 16:05:26 +0200

    tests and examples

---
+++ internal/tests/postgresql_resource_test.go
@@ -0,0 +1,78 @@
+package tests
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/nl-ams-dspc/terraform-provider-dspc/internal/client"
+	"github.com/stretchr/testify/suite"
+)
+
+type PostgreSQLResourceSuite struct {
+	MockProvider
+}
+
+func TestPostgreSQLProvisioning(t *testing.T) {
+	suite.Run(t, new(PostgreSQLResourceSuite))
+}
+
+func (s *PostgreSQLResourceSuite) TestAccPostgreSQLResource() {
+	state := client.PostgreSQLInstance{
+		Name:    "test-postgres",
+		Size:    "1Gi",
+		Version: client.DatabaseVersionPostgres17,
+		VPC:     "test-vpc",
+	}
+
+	s.Handlers = MockResponses{
+		"POST " + BuildTestPath("network", "/databases"): func() MockResponse {
+			return MockResponse{
+				ResponseCode: http.StatusCreated,
+				ResponseBody: state,
+			}
+		},
+		"GET " + BuildTestPath("network", "/databases/test-postgres"): func() MockResponse {
+			return MockResponse{
+				ResponseCode: http.StatusOK,
+				ResponseBody: state,
+			}
+		},
+		"DELETE " + BuildTestPath("network", "/databases/test-postgres"): func() MockResponse {
+			return MockResponse{
+				ResponseCode: http.StatusNoContent,
+				ResponseBody: nil,
+			}
+		},
+	}
+
+	resource.Test(s.T(), resource.TestCase{
+		ProtoV6ProviderFactories: TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and read test
+			{
+				Config: TestProvider(s.Server.URL, s.AuthServer.URL) + `
+resource "dspc_postgresql" "test" {
+	name    = "test-postgres"
+	size    = "1Gi"
+	version = "POSTGRES_17"
+	vpc     = "test-vpc"
+}
+`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("dspc_postgresql.test", "name", "test-postgres"),
+					resource.TestCheckResourceAttr("dspc_postgresql.test", "size", "1Gi"),
+					resource.TestCheckResourceAttr("dspc_postgresql.test", "version", "POSTGRES_17"),
+					resource.TestCheckResourceAttr("dspc_postgresql.test", "vpc", "test-vpc"),
+				),
+			},
+			// ImportState testing
+			{
+				ResourceName:  "dspc_postgresql.test",
+				ImportState:   true,
+				ImportStateId: "test-postgres",
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
