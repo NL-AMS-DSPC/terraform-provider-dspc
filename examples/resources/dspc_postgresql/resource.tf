4d0a4daf263b7156ef78f4e653027566f17bbd7d
Michael Boke <michael@mbict.nl>
2026-06-05 12:36:06 +0200

    ASCP-946 other fixed test

--- examples/resources/dspc_postgresql/resource.tf
+++ examples/resources/dspc_postgresql/resource.tf
@@ -1,17 +1,17 @@
 # Create a PostgreSQL instance
 resource "dspc_postgresql" "example" {
   name    = "my-postgres-db"
-  size    = "1Gi"
+  sku_size    = "gp-2"
   version = "POSTGRES_17"
-  vpc     = "my-vpc"
+  vpc_id     = "00000000-0000-0000-0000-000000000000"
 }
 
 # Create a PostgreSQL instance with tags
 resource "dspc_postgresql" "tagged" {
   name    = "my-tagged-postgres"
-  size    = "2Gi"
+  sku_size    = "gp-4"
   version = "POSTGRES_16"
-  vpc     = "my-vpc"
+  vpc_id     = "00000000-0000-0000-0000-000000000000"
 
   tags = [
     {
